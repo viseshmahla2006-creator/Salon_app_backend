ed0449e8a80dfcd790a3df2d7495516cd2e30ab8
viseshmahla2006-creator <viseshmahla2006@gmail.com>
2026-09-22 12:53:16 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -3,10 +3,37 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema(
   {
     googleId: { type: String, unique: true, sparse: true },
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true, lowercase: true },
-    phone: { type: String },
-    role: { type: String, enum: ["customer", "owner"], required: true },
+
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      unique: true,
+      sparse: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    phone: {
+      type: String,
+      unique: true,
+      sparse: true,
+      trim: true,
+    },
+
+    passwordHash: {
+      type: String,
+    },
+
+    role: {
+      type: String,
+      enum: ["customer", "owner"],
+      required: true,
+    },
   },
   { timestamps: true }
 );
